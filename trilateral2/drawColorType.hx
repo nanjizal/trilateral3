c9d570b2fa432fdef2d7ee0b5f2ae486249c533f
nanjizal <nanjizal@users.noreply.github.com>
2019-12-24 12:26:04 +0000

    Update and rename trilateral2/draw/drawColorType.hx to trilateral2/drawColorType.hx

--- trilateral2/drawColorType.hx
+++ trilateral2/drawColorType.hx
@@ -1,4 +1,4 @@
-package trilateral2.draw; 
+package trilateral2; 
 typedef ColorType = {
     var cornerColors: ( colorA: Int, colorB: Int, colorC: Int ) -> Void;
     var colorTriangles: ( color: Int, times: Int ) -> Void;
