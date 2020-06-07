bee7f6c5bd20a0786093fc96ce757c0c7f3a8eef
nanjizal <none>
2020-06-07 02:38:56 +0100

    put stuff in src removed tests stuff as was not really using

--- src/trilateral3/DocTrilateral3.hx
+++ src/trilateral3/DocTrilateral3.hx
@@ -1,12 +1,14 @@
 package trilateral3;
 import trilateral3.Trilateral;
-//import trilatelal3.dev.*;  TODO: re-add VertexObject
+import trilateral3.color.*;
 import trilateral3.drawing.*;
 import trilateral3.geom.*;
 import trilateral3.geom.flat.*;
 import trilateral3.geom.obj.*;
-import trilateral3.iter.*;
+import trilateral3.geom.math.*;
 import trilateral3.math.*;
+import trilateral3.matrix.*;
+import trilateral3.nodule.*;
 import trilateral3.shape.*;
 import trilateral3.shape.xtra.*;
 import trilateral3.structure.*;
